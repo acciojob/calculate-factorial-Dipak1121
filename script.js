0bd4800a17575bd0cdd0fe1d71f13c74eecdaaa3
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-29 10:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let num = prompt("Enter the Number");
+
+alert(`The factorial of ${num} is ${fact(num)}`);
+function fact(num) {
+	if(num == 1){
+		return 1;
+	}
+	return (num*fact(num-1));
+}
